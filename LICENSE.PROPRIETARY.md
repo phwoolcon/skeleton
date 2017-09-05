07ee5f06080a578c8d9e348a3e5c436db4969799
Christopher CHEN <fishdrowned@gmail.com>
2017-09-05 20:04:50 +0800

    feat: add proprietary license

---
+++ LICENSE.PROPRIETARY.md
@@ -0,0 +1,9 @@
+# PROPRIETARY LICENSE
+
+Copyright (C) :licensor - All Rights Reserved
+
+> Unauthorized use prohibited  
+> Unauthorized copying of this file, via any medium is strictly prohibited  
+> Proprietary and confidential
+>
+> Written by :author_name <:author_email>, {yyyy}
--- prefill-phwoolcon.php
+++ prefill-phwoolcon.php
@@ -1,6 +1,7 @@
 <?php
 include __DIR__ . '/prefill.php';
 
+echo "\n";
 echo "----------------------------------------------------------------------\n";
 echo "Please, provide the following information:\n";
 echo "----------------------------------------------------------------------\n";
@@ -10,7 +11,8 @@ $phwoolconFields = [
         'The git repository of the package',
         'git@github.com:{package_vendor}/{package_name}.git',
     ],
-    'license' => ['Choose license', '1 - APACHE 2.0, 2 - MIT', '1'],
+    'license' => ['Choose license', '1 - APACHE 2.0, 2 - MIT, 3 - Proprietary', '1'],
+    'licensor' => ['Licensor', 'The owner of this package', '{package_vendor}'],
 ];
 $phwoolconReplacements = [
     ':vendor' => function () use (&$values) {
@@ -22,8 +24,16 @@ $phwoolconReplacements = [
     'phwoolcon/skeleton' => function () use (&$values) {
         return "{$values['package_vendor']}/{$values['package_name']}";
     },
+    ':licensor' => function () use (&$values) {
+        return $values['licensor'];
+    },
     ':license' => function () use (&$values) {
-        return $values['license'] == 2 ? 'MIT' : 'Apache-2.0';
+        $licenses = [
+            1 => 'Apache-2.0',
+            2 => 'MIT',
+            3 => 'Proprietary',
+        ];
+        return isset($licenses[$values['license']]) ? $licenses[$values['license']] : $licenses[1];
     },
     '{yyyy}' => function () {
         return date('Y');
@@ -32,15 +42,28 @@ $phwoolconReplacements = [
         return $values['git_repo'];
     },
     'The MIT License (MIT). Please see [License File](LICENSE.md)' => function () use (&$values) {
-        return $values['license'] == 2 ? 'The MIT License (MIT). Please see [License File](LICENSE.md)' :
-            'The Apache License, Version 2.0. Please see [License File](LICENSE.md)';
+        $licenses = [
+            1 => 'The Apache License, Version 2.0. Please see [License File](LICENSE.md)',
+            2 => 'The MIT License (MIT). Please see [License File](LICENSE.md)',
+            3 => 'Proprietary License. Please see [License File](LICENSE.md)',
+        ];
+        return isset($licenses[$values['license']]) ? $licenses[$values['license']] : $licenses[1];
     },
     'license-MIT-brightgreen.svg' => function () use (&$values) {
-        return $values['license'] == 2 ? 'license-MIT-brightgreen.svg' : 'license-Apache%202.0-brightgreen.svg';
+        $licenses = [
+            1 => 'license-Apache%202.0-brightgreen.svg',
+            2 => 'license-MIT-brightgreen.svg',
+            3 => 'license-Proprietary-brightgreen.svg',
+        ];
+        return isset($licenses[$values['license']]) ? $licenses[$values['license']] : $licenses[1];
     },
 ];
 
 foreach ($phwoolconFields as $f => $field) {
+    if ($f == 'licensor' && $values['license'] != 3) {
+        $values['licensor'] = '';
+        continue;
+    }
     $default = isset($field[COL_DEFAULT]) ? interpolate($field[COL_DEFAULT], $values) : '';
     $prompt = sprintf('%s%s%s: ', $field[COL_DESCRIPTION], $field[COL_HELP] ? ' (' . $field[COL_HELP] . ')' : '',
         $field[COL_DEFAULT] !== '' ? ' [' . $default . ']' : '');
@@ -63,10 +86,16 @@ foreach ($files as $f) {
 }
 
 // Apply license
-if ($values['license'] == 2) {
+if ($values['license'] == 3) {
+    unlink(__DIR__ . '/LICENSE.md');
     unlink(__DIR__ . '/LICENSE.APACHE2.md');
+    rename(__DIR__ . '/LICENSE.PROPRIETARY.md', __DIR__ . '/LICENSE.md');
+} elseif ($values['license'] == 2) {
+    unlink(__DIR__ . '/LICENSE.APACHE2.md');
+    unlink(__DIR__ . '/LICENSE.PROPRIETARY.md');
 } else {
     unlink(__DIR__ . '/LICENSE.md');
+    unlink(__DIR__ . '/LICENSE.PROPRIETARY.md');
     rename(__DIR__ . '/LICENSE.APACHE2.md', __DIR__ . '/LICENSE.md');
 }
 
